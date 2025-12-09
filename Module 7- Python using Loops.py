93913871a8e55b7f9313ce6e799786686b6aee95
madisonknguyen-dotcom <madisonknguyen@gmail.com>
2025-12-09 14:52:37 -0600

    Add files via upload

---
+++ Module 7- Python using Loops.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+for number in numbers:
+    # Check if the number is even
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
